2d2f8df021335ca2ad95e052ecb56a79dbae4a39
Lisa Dezaki <lisadezaki@gmail.com>
2025-10-01 14:53:06 -0700

    Dismissible Toasts

--- resources/js/Components/UI/Toast.svelte
+++ resources/js/Components/UI/Toast.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount, onDestroy } from 'svelte';
+
 	import { Inline } from '@/Components/Core'
 	import Button from '@/Components/UI/Button.svelte'
 	import Icon from '@/Components/UI/Icon.svelte'
@@ -8,7 +10,24 @@
 		icon,
 		message,
 		success,
+		onDismiss
 	} = $props()
+
+	let visible = $state(false)
+	let timeoutId
+
+	function dismiss() {
+		visible = false
+	}
+
+	onMount(() => {
+		visible = true
+		timeoutId = setTimeout(dismiss,5000)
+	});
+
+	onDestroy(() => {
+		clearTimeout(timeoutId);
+	})
 
 </script>
 
@@ -16,26 +35,26 @@
 
 
 
-{#if success}
+{#if visible && success}
 	<Inline class="toast backdrop-blur bg-accent-gradient border border-accent-strong fixed bottom-8 left-1/2 p-1.5 pl-3 rounded-full shadow-xl text-white translate-x-[-50%] z-50">
 		<Icon name={icon || 'Check'} size="md" weight="bold" />
 		<span class="font-medium">{success}</span>
-		<Button icon="X" iconSize="sm" iconWeight="bold" style="plain" theme="danger" class="bg-accent-gradient-flip ml-3 rounded-full" />
+		<Button onclick={dismiss} icon="X" iconSize="sm" iconWeight="bold" style="plain" theme="danger" class="bg-accent-gradient-flip ml-3 rounded-full" />
 	</Inline>
 {/if}
 
-{#if message}
+{#if visible && message}
 	<Inline class="toast bg-neutral-softer backdrop-blur-sm border border-neutral-soft fixed bottom-8 left-1/2 p-1.5 pl-3 rounded-full shadow-xl text-neutral translate-x-[-50%] z-50">
 		<Icon name={icon || 'Info'} size="md" weight="fill" />
 		<span class="font-medium">{message}</span>
-		<Button icon="X" iconSize="sm" iconWeight="light" style="plain" theme="danger" class="ml-3 rounded-full" />
+		<Button onclick={dismiss} icon="X" iconSize="sm" iconWeight="light" style="plain" theme="danger" class="ml-3 rounded-full" />
 	</Inline>
 {/if}
 
-{#if error}
+{#if visible && error}
 	<Inline class="toast bg-danger-gradient border border-danger-strong fixed bottom-8 left-1/2 p-1.5 pl-3 rounded-full shadow-xl text-white translate-x-[-50%] z-50">
 		<Icon name={icon || 'SmileySad'} size="md" weight="fill" />
 		<span class="font-medium">{error}</span>
-		<Button icon="X" iconSize="sm" iconWeight="light" style="plain" theme="danger" class="ml-3 rounded-full" />
+		<Button onclick={dismiss} icon="X" iconSize="sm" iconWeight="light" style="plain" theme="danger" class="ml-3 rounded-full" />
 	</Inline>
 {/if}
